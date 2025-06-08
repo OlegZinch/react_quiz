e9453cd38cc0f7f35522edbac4b861e68545d70e
Oleh Zinchenko <olegzinchenko1983@gmail.com>
2025-06-09 00:19:24 +0100

    added restarting a quiz

--- src/components/App.js
+++ src/components/App.js
@@ -43,6 +43,8 @@ function reducer(state, action) {
         highscore:
           state.points > state.highscore ? state.points : state.highscore,
       }
+    case 'restart':
+      return { ...state, status: 'ready', index: 0, answer: null, points: 0 }
 
     default:
       throw new Error('Action unknow')
@@ -100,6 +102,7 @@ export default function App() {
             points={points}
             maxPossiblePoints={maxPossiblePoints}
             highscore={highscore}
+            dispatch={dispatch}
           />
         )}
       </Main>
